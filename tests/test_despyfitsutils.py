a810653a32241f5cea0f765a4ba0975449672a9c
Doug Friedel <friedel@illinois.edu>
2019-10-23 16:22:13 -0500

    initial test of combine cats

--- tests/test_despyfitsutils.py
+++ tests/test_despyfitsutils.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python2
 
 import unittest
+import sys
+import copy
 
-class TestFitsutils(unittest.TestCase):
-    def test_combine(self):
-        pass
+import combine_cats as ccats
+#class TestFitsutils(unittest.TestCase):
+#    def test_combine(self):
+#        pass
+
+class TestCobmineCats(unittest.TestCase):
+    def test_commandline(self):
+        temp = copy.deepcopy(sys.argv)
+        sys.argv = ["combine_cats.py",
+                    '--outcat',
+                    'MyNewCat.fits',
+                    '--incats',
+                    '/var/lib/jenkins/test_data/cat/D00526117_r_c23_r4048p01_scampcat.fits,/var/lib/jenkins/test_data/cat/D00526117_r_c23_r4048p01_scampcat.fits']
+        ccats.main()
+        sys.argv = temp
 
 if __name__ == '__main__':
     unittest.main()
