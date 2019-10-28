724fc3025028507d4fdfd97c22e20c8bef582e3e
Doug Friedel <friedel@illinois.edu>
2019-10-28 09:45:04 -0500

    tests for fits_special_metadata

--- tests/test_despyfitsutils.py
+++ tests/test_despyfitsutils.py
@@ -11,6 +11,8 @@ from StringIO import StringIO
 
 import combine_cats as ccats
 import split_head as splith
+import despyfitsutils.fits_special_metadata as fsm
+import pyfits
 #class TestFitsutils(unittest.TestCase):
 #    def test_combine(self):
 #        pass
@@ -149,6 +151,43 @@ class TestSplitScampHead(unittest.TestCase):
         sys.argv = temp
 
 
+class TestFitsSpecialMetadata(unittest.TestCase):
+    testfile = ROOT + 'raw/test_raw.fits.fz'
+    def test_func_band(self):
+        self.assertEqual(fsm.func_band(self.testfile), 'g')
+        self.assertEqual(fsm.func_band(self.testfile, pyfits.open(self.testfile), 'PRIMARY'), 'g')
+
+    def test_func_camsym(self):
+        self.assertEqual(fsm.func_camsym(self.testfile), 'D')
+        self.assertEqual(fsm.func_camsym(self.testfile, pyfits.open(self.testfile), 'PRIMARY'), 'D')
+
+    def test_func_nite(self):
+        self.assertEqual(fsm.func_nite(self.testfile), '20161018')
+        self.assertEqual(fsm.func_nite(self.testfile, pyfits.open(self.testfile), 'PRIMARY'), '20161018')
+
+    def test_func_objects(self):
+        self.assertRaises(KeyError, fsm.func_objects, self.testfile)
+        self.assertEqual(fsm.func_objects(self.testfile, pyfits.open(self.testfile), 1), 4146)
+
+    def test_func_field(self):
+        self.assertEqual(fsm.func_field(self.testfile), '-159-521')
+        self.assertEqual(fsm.func_field(self.testfile, pyfits.open(self.testfile), 'PRIMARY'), '-159-521')
+
+    def test_func_radec(self):
+        self.assertAlmostEqual(fsm.func_radeg(self.testfile), 345.6291, 4)
+        self.assertAlmostEqual(fsm.func_radeg(self.testfile, pyfits.open(self.testfile), 'PRIMARY'), 345.6291, 4)
+
+    def test_func_tradec(self):
+        self.assertAlmostEqual(fsm.func_tradeg(self.testfile), 345.628829, 6)
+        self.assertAlmostEqual(fsm.func_tradeg(self.testfile, pyfits.open(self.testfile)), 345.628829, 6)
+
+    def test_func_decdec(self):
+        self.assertAlmostEqual(fsm.func_decdeg(self.testfile), -51.732708, 6)
+        self.assertAlmostEqual(fsm.func_decdeg(self.testfile, pyfits.open(self.testfile), 'PRIMARY'), -51.732708, 6)
+
+    def test_func_tdecdec(self):
+        self.assertAlmostEqual(fsm.func_tdecdeg(self.testfile), -51.732137, 6)
+        self.assertAlmostEqual(fsm.func_tdecdeg(self.testfile, pyfits.open(self.testfile)), -51.732137, 6)
 
 if __name__ == '__main__':
     unittest.main()
