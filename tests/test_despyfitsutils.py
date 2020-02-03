132a2633e64064b928bdb10d95a4c36f76722f2f
Doug Friedel <friedel@illinois.edu>
2020-02-03 12:17:05 -0600

    added tests for printHeader.py

--- tests/test_despyfitsutils.py
+++ tests/test_despyfitsutils.py
@@ -13,6 +13,7 @@ import combine_cats as ccats
 import split_head as splith
 import despyfitsutils.fits_special_metadata as fsm
 from astropy.io import fits
+import printHeader as phdr
 #class TestFitsutils(unittest.TestCase):
 #    def test_combine(self):
 #        pass
@@ -188,6 +189,48 @@ class TestFitsSpecialMetadata(unittest.TestCase):
     def test_func_tdecdec(self):
         self.assertAlmostEqual(fsm.func_tdecdeg(self.testfile), -51.732137, 6)
         self.assertAlmostEqual(fsm.func_tdecdeg(self.testfile, fits.open(self.testfile)), -51.732137, 6)
+
+class Test_printHeader(unittest.TestCase):
+    testfile = ROOT + 'raw/test_raw.fits.fz'
+    outfile = 'test.dat'
+    def test_main_errors(self):
+        temp = copy.deepcopy(sys.argv)
+        sys.argv = ['printHeader.py', '-o', '/x/y/z', self.testfile]
+        self.assertRaises(SystemExit, phdr.main)
+
+        sys.argv = ['printHeader.py','-x','blob', self.testfile]
+        self.assertRaises(OSError, phdr.main)
+        sys.argv = copy.deepcopy(temp)
+
+    def test_file_output(self):
+        temp = copy.deepcopy(sys.argv)
+        sys.argv = ['printHeader.py', '-o', self.outfile, self.testfile]
+        phdr.main()
+        sys.argv = copy.deepcopy(temp)
+        f = open(self.outfile, 'r')
+        items = {'SB_ID': False, 'INSTANCE': False, 'SEQID': False}
+        for line in f.readlines():
+            for key in items.keys():
+                if key in line:
+                    items[key] = True
+                    break
+        for key in items.keys():
+            self.assertTrue(items[key])
+        f.close()
+        os.unlink(self.outfile)
+
+    def test_stdout_output(self):
+        temp = copy.deepcopy(sys.argv)
+        sys.argv = ['printHeader.py', self.testfile]
+        output = ''
+        with capture_output() as (out, _):
+            phdr.main()
+            output = out.getvalue().strip()
+            items = ['SB_ID', 'INSTANCE','SEQID']
+            for it in items:
+                self.assertTrue(it in output)
+
+        sys.argv = copy.deepcopy(temp)
 
 if __name__ == '__main__':
     unittest.main()
